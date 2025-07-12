cb9275c12c3c30368faf18080089ed3434e3e11c
yongjun.xiao <yongjun.xiao@ximalaya.com>
2025-07-12 11:17:17 +0800

    【主站播放页-评论区】展示儿童VIP标识

--- examples/get_kuaishou_cookie.py
+++ examples/get_kuaishou_cookie.py
@@ -1,5 +1,11 @@
 import asyncio
+import sys
+import os
 from pathlib import Path
+
+# 添加项目根目录到系统路径
+current_dir = Path(__file__).parent.parent.resolve()
+sys.path.append(str(current_dir))
 
 from conf import BASE_DIR
 from uploader.ks_uploader.main import ks_setup
